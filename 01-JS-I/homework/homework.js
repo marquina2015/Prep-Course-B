53b468cd9c499a75dbc408609ba55b3a2c6286b4
marquina2015 <115118762+marquina2015@users.noreply.github.com>
2022-10-17 10:43:57 -0500

    homework JS I

---
+++ 01-JS-I/homework/homework.js
@@ -0,0 +1,30 @@
+// En estas primeras 6 preguntas, reemplaza `null` por la respuesta
+
+// Crea una variable "string", puede contener lo que quieras:
+const nuevaString = 'hola mundo';
+
+// Crea una variable numérica, puede ser cualquier número:
+const nuevoNum = 5;
+
+// Crea una variable booleana:
+const nuevoBool = false;
+
+// Resuelve el siguiente problema matemático:
+const nuevaResta = 10 - 5 === 5;
+
+// Resuelve el siguiente problema matemático:
+const nuevaMultiplicacion = 10 * 4 === 40 ;
+
+// Resuelve el siguiente problema matemático:
+const nuevoModulo = 21 % 5 === 1;
+
+// No modificar nada debajo de esta línea
+// --------------------------------
+module.exports = {
+  nuevaString,
+  nuevoNum,
+  nuevoBool,
+  nuevaResta,
+  nuevaMultiplicacion,
+  nuevoModulo,
+};
